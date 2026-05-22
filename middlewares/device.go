6f282991efa0f5d0ffe7cb69d7c8a01bf98b3207
unknown <quocbao.job106240@gmail.com>
2026-05-23 00:50:58 +0700

    feat/ add device middleware

--- middlewares/device.go
+++ middlewares/device.go
@@ -1,1 +1,36 @@
 package middlewares
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type contextKey string
+
+const DeviceIDKey contextKey = "device_id"
+
+func (m *Middleware) DeviceRequired() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			deviceID := c.Request().Header.Get("X-Device-ID")
+			if deviceID == "" {
+				return echo.NewHTTPError(
+					http.StatusBadRequest,
+					"X-Device-ID header is required",
+				)
+			}
+
+			ctx := context.WithValue(
+				c.Request().Context(),
+				DeviceIDKey,
+				deviceID,
+			)
+
+			c.SetRequest(c.Request().WithContext(ctx))
+
+			return next(c)
+		}
+	}
+}
