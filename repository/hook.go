f20ab43d97120053134b1299c7428cbd17c4bae6
unknown <quocbao.job106240@gmail.com>
2026-05-23 00:51:43 +0700

    refactor: GORM custom hook

--- repository/hook.go
+++ repository/hook.go
@@ -11,13 +11,11 @@ type Repository[T interface{}] struct {
 	db *gorm.DB
 }
 
-func (r Repository[T]) FindOne(ctx context.Context, args ...interface{}) (*T, error) {
+func (r *Repository[T]) Take(ctx context.Context, args ...interface{}) (*T, error) {
 	record := new(T)
 
-	tx := r.db.WithContext(ctx)
-	tx = applyArgs(tx, args...)
+	err := r.Resolve(ctx).Take(record, args...).Error
 
-	err := tx.First(record).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) {
 		return nil, nil
 	}
@@ -25,20 +23,19 @@ func (r Repository[T]) FindOne(ctx context.Context, args ...interface{}) (*T, er
 	return record, err
 }
 
-func (r Repository[T]) GetAll(ctx context.Context, args ...interface{}) ([]T, error) {
-	var records []T
+func (r *Repository[T]) GetAll(ctx context.Context, args ...interface{}) ([]*T, error) {
+	var records []*T
 
-	tx := r.db.WithContext(ctx)
-	tx = applyArgs(tx, args...)
+	err := r.Resolve(ctx).Find(&records, args...).Error
 
-	if err := tx.Find(&records).Error; err != nil {
+	if err != nil {
 		return nil, err
 	}
 
 	return records, nil
 }
 
-func (r Repository[T]) Create(ctx context.Context, entity *T) (*T, error) {
+func (r *Repository[T]) Create(ctx context.Context, entity *T) (*T, error) {
 	if err := r.Resolve(ctx).Create(entity).Error; err != nil {
 		return nil, err
 	}
@@ -46,48 +43,49 @@ func (r Repository[T]) Create(ctx context.Context, entity *T) (*T, error) {
 	return entity, nil
 }
 
-func (r Repository[T]) Update(ctx context.Context, entity *T) (*T, error) {
-	if err := r.Resolve(ctx).Save(entity).Error; err != nil {
+func (r *Repository[T]) Update(ctx context.Context, entity *T) (*T, error) {
+	if err := r.Resolve(ctx).Updates(entity).Error; err != nil {
 		return nil, err
 	}
 
 	return entity, nil
 }
 
-func (r Repository[T]) UpdateWhere(ctx context.Context, values interface{}, args ...interface{}) error {
-	tx := r.db.WithContext(ctx)
-	tx = applyArgs(tx, args...)
+//func (r Repository[T]) UpdateWhere(ctx context.Context, values interface{}, args ...interface{}) error {
+//	tx := r.Resolve(ctx)
+//	tx = applyArgs(tx, args...)
+//
+//	return tx.Model(new(T)).Updates(values).Error
+//}
 
-	return tx.Model(new(T)).Updates(values).Error
+func (r *Repository[T]) Delete(ctx context.Context, args ...interface{}) error {
+	if len(args) == 0 {
+		return errors.New("delete conditions required")
+	}
+
+	return r.Resolve(ctx).Delete(new(T), args...).Error
 }
 
-func (r Repository[T]) Delete(ctx context.Context, args ...interface{}) error {
-	tx := r.Resolve(ctx)
-	tx = applyArgs(tx, args...)
-
-	return tx.Delete(new(T)).Error
+func (r *Repository[T]) Pluck(ctx context.Context, column string, dest any) error {
+	return r.Resolve(ctx).Model(new(T)).Pluck(column, dest).Error
 }
 
-func (r Repository[T]) Pluck(ctx context.Context, column string, dest interface{}, args ...interface{}) error {
-	tx := r.db.WithContext(ctx)
-	tx = applyArgs(tx, args...)
+func (r *Repository[T]) Exists(ctx context.Context, args ...interface{}) (bool, error) {
+	record := new(T)
 
-	return tx.Model(new(T)).Pluck(column, dest).Error
-}
+	err := r.Resolve(ctx).Select("1").Take(record, args...).Error
 
-func (r Repository[T]) Exists(ctx context.Context, args ...interface{}) (bool, error) {
-	var count int64
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, nil
+	}
 
-	tx := r.db.WithContext(ctx)
-	tx = applyArgs(tx, args...)
-
-	if err := tx.Model(new(T)).Count(&count).Error; err != nil {
+	if err != nil {
 		return false, err
 	}
 
-	return count > 0, nil
+	return true, nil
 }
 
 func (r Repository[T]) WithContext(ctx context.Context) *gorm.DB {
-	return r.db.WithContext(ctx)
+	return r.Resolve(ctx)
 }
